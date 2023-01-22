6d0590064c0f59592821b85fb6e373460ba1cee4
MUAHMMEDJASEEM <94896613+MUAHMMEDJASEEM@users.noreply.github.com>
2023-01-22 07:55:06 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './components/NavBar';
+import UserForm from './components/UserForm';
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <UserForm />
+    </div>
+  )
+}
+
+export default App;
